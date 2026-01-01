bd69e6a41f2cbbf34894a091c02ae05feb5dbd17
Wh4tz <151379163+Nu-man-71@users.noreply.github.com>
2026-01-01 21:54:57 +0545

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,15 @@
+const title = document.querySelector('.title')
+const text = 'Cutie pie...  :-)'.split('')
+for (let index = 0; index < text.length; index++) {
+  if (text[index] !== ' ') {
+    title.innerHTML += `<span>${text[index]}<span/>`
+  } else {
+    title.innerHTML += `<span style='margin-right: 20px;'><span/>`
+  }
+}
+
+const textElements = document.querySelectorAll('.title span');
+textElements.forEach((element) => {
+  const randomDelay = Math.random() * 3; // Generates a random delay between 0 and 3 seconds.
+  element.style.animationDelay = `${randomDelay}s`;
+});
